7534acf005c3fb7c47ba20bd4159a4b76e981d70
kimseasok <basicblogtalk@gmail.com>
2018-12-22 04:53:35 -0500

    Serving bootstrap static file from node module folder

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var app = express();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
+//Serve static file from node modules
+
+app.use('/css/', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(__dirname, '/node_modules/jquery/dist'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 })
