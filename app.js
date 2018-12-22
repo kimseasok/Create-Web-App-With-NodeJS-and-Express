5bc7c7de08598fec1e8261c5e7adf519c6078c57
kimseasok <basicblogtalk@gmail.com>
2018-12-22 03:40:29 -0500

    Setup express in app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!...');
+})
+
+app.listen(3000, () => {
+    console.log('Node listening on port 3000');
+});
