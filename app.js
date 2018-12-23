8d3e3f2dc592d23ae03eaae89c2188a6a10e1d59
kimseasok <basicblogtalk@gmail.com>
2018-12-23 00:22:30 -0500

    Config nodemon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
@@ -19,6 +19,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`Node listening on port ${chalk.green(3000)}`);
+app.listen(port, () => {
+  debug(`Node listening at port ${chalk.green(port)}`);
 });
