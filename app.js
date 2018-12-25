c90a8e7402e8aa1ec0f23414f637173505b084dd
kimseasok <basicblogtalk@gmail.com>
2018-12-25 02:36:39 -0500

    Connect to mssql and requery books

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const config = {
+  user: 'SA',
+  password: 'Op3nmymi@l',
+  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+  database: 'node_test',
+
+  options: {
+    encrypt: false // Use this if you're on Windows Azure
+  }
+};
+
+sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,35 +2,43 @@ const express = require('express');
 
 const booksRouter = express.Router();
 
-const books = [
-  {
-    title: 'War and Peace',
-    genre: 'Historical Fiction',
-    author: 'Lev Nikolayevich Tolstory',
-    read: false
-  },
-  {
-    title: 'Les Miserable',
-    genre: 'Science Fiction',
-    author: 'H. G. Wells',
-    read: false
-  },
-  {
-    title: 'A Jouney in the Center of the Earth',
-    genre: 'Science Fiction',
-    author: 'Jules Verne',
-    read: false
-  }
-];
+const sql = require('mssql');
+
+// const debug = require('debug')('app:bookRoutes');
+
 
 function router(nav) {
+  const books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstory',
+      read: false
+    },
+    {
+      title: 'Les Miserable',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false
+    },
+    {
+      title: 'A Jouney in the Center of the Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false
+    }
+  ];
   booksRouter.route('/')
     .get((req, res) => {
-      res.render('books', {
-        nav,
-        title: 'Books',
-        books
-      });
+      (async function query() {
+        const request = new sql.Request();
+        const result = await request.query('select * from books');
+        res.render('books', {
+          nav,
+          title: 'Books',
+          books: result.recordset
+        });
+      }());
     });
 
   booksRouter.route('/:id')
