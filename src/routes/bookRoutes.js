d25c8bf9a7e44be64805b2683513c40fbba20535
kimseasok <basicblogtalk@gmail.com>
2018-12-25 02:50:34 -0500

    Add query for single view

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -8,29 +8,29 @@ const sql = require('mssql');
 
 
 function router(nav) {
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstory',
-      read: false
-    },
-    {
-      title: 'Les Miserable',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false
-    },
-    {
-      title: 'A Jouney in the Center of the Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    }
-  ];
+  // const books = [
+  //   {
+  //     title: 'War and Peace',
+  //     genre: 'Historical Fiction',
+  //     author: 'Lev Nikolayevich Tolstory',
+  //     read: false
+  //   },
+  //   {
+  //     title: 'Les Miserable',
+  //     genre: 'Science Fiction',
+  //     author: 'H. G. Wells',
+  //     read: false
+  //   },
+  //   {
+  //     title: 'A Jouney in the Center of the Earth',
+  //     genre: 'Science Fiction',
+  //     author: 'Jules Verne',
+  //     read: false
+  //   }
+  // ];
   booksRouter.route('/')
     .get((req, res) => {
-      (async function query() {
+      (async function queryBooks() {
         const request = new sql.Request();
         const result = await request.query('select * from books');
         res.render('books', {
@@ -44,14 +44,16 @@ function router(nav) {
   booksRouter.route('/:id')
     .get((req, res) => {
       const { id } = req.params;
-
-      res.render('book', {
-        nav,
-        title: 'Single Book',
-        book: books[id]
-      });
+      (async function querybook() {
+        const request = new sql.Request();
+        const result = await request.query(`select * from books where book_id=${id}`);
+        res.render('book', {
+          nav,
+          title: 'Single Book',
+          book: result.recordset[0]
+        });
+      }());
     });
-
   return booksRouter;
 }
 
