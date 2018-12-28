3680ad489a643bdc6f619cb83133ebd39ae71dc1
kimseasok <basicblogtalk@gmail.com>
2018-12-27 22:53:02 -0500

    Add body-parser for authRouter

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
+// const sql = require('mssql');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3030 || 3000;
 
-const config = {
-  user: 'SA',
-  password: 'Op3nmymi@l',
-  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
-  database: 'node_test',
+// const config = {
+//   user: 'SA',
+//   password: 'Op3nmymi@l',
+//   server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+//   database: 'node_test',
 
-  options: {
-    encrypt: false // Use this if you're on Windows Azure
-  }
-};
+//   options: {
+//     encrypt: false // Use this if you're on Windows Azure
+//   }
+// };
 
-sql.connect(config).catch(err => debug(err));
+// sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public/')));
 
 // Serve static file from node modules
@@ -41,9 +44,11 @@ const nav = [
 
 const booksRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/AuthRoutes')(nav);
 
 app.use('/books', booksRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render('index', {
